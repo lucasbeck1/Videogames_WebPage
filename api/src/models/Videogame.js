4762b91e7ec8dc90c48ccb170e179d456f3b261f
lucasbeck1 <lucasbeckford1@gmail.com>
2022-09-14 22:35:31 -0300

    Day-ONE_1

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,41 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
+module.exports = (s) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    name: {
+  s.define('videogame', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4, 
+      alowNull: false,
+      primaryKey: true 
+    },
+    name:{
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description:{
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    released:{
+      type: DataTypes.STRING,
+    },
+    rating:{
+      type: DataTypes.FLOAT,
+    },
+    platforms:{
+      type: DataTypes.STRING,
+      allowNull: null
+    },
+    image:{
+      type: DataTypes.STRING
+    },
+    createdInDatabase:{
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
+  },{
+    timestamps: false
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,77 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const {Videogame, Genre} = require('../db')
+const axios = require('axios');
+const { get } = require('superagent');
+const {API_KEY} = process.env;
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+
+const getApiInfo = async () => {
+    const firstHundredGames = [];
+    for (let i = 1; i < 6; i++) {
+        let apiInfo = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page=${i}`)
+        apiInfo.data.results.map(e => {
+            firstHundredGames.push({
+                id : e.id,
+                name: e.name,
+                img: e.background_image,
+                genres: e.genres.map(e => e.name).join(', '),
+                released: e.released,
+                rating: e.rating,
+                platform: e.platforms.map((e) => e.platform.name).join(', ')
+            })
+        })
+    }
+    return firstHundredGames;
+};
+
+const getDbInfo = async () => {
+    const dbinfo = await Videogame.findAll({
+        include:{
+            model: Genre,
+            attribute: ['name'],
+            through: {
+                attributes: [],
+            }
+        }
+    })
+    return dbinfo;
+}
+
+const getAllGames = async () => {
+    const apiGames = await getApiInfo();
+    const DbGames = await getDbInfo();
+    return (apiGames.concat(DbGames));
+}
+
+
+router.get('/videogames', async function(req, res, next){
+    const allGames = await getAllGames();
+    const {name} = req.query;
+    if(!name){
+        res.json(allGames);
+    }else{
+        allGames.filter(game => game.name.includes(name));
+        res.json(allGames);
+    }
+
+});
+
+router.get('/videogames/:idVideogame', async function(req, res, next){
+    const allGames = await getAllGames();
+    const {idVideogame} = req.params;
+    const findGameById = allGames.find(game => game.id === idVideogame);
+    
+});
+
+
+
 
 
 module.exports = router;
